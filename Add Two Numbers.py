de985a03c18830e41874f8bb7501a49d54f9fcda
Kommu Sai Praneeth <kommusaipraneeth@gmail.com>
2023-12-27 15:05:35 -0500

    Code1

---
+++ Add Two Numbers.py
@@ -0,0 +1,17 @@
+# Solution 1 (with Pre-defined variables):
+
+num1 = 12
+num2 = 23
+
+sum = num1 + num2
+# print(num1 + num2)
+# print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
+
+# Solution 2 (with user-input):
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum = num1 + num2
+
+print(sum)
